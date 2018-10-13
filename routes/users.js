99cff5180f3aad9db1233a384e3eabc1738d0d39
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-10-13 11:45:26 -0400

    Add new routes to index and user routes, created templates for both.

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/bob', function(req, res, next) {
+  res.render('bob', { title: 'Bob\'s Page' });
+});
+
 module.exports = router;
